b46579962a0f1b7e2c97df2b8367734182ddb370
NomerMahmoud <mahmoudehab00@gmail.com>
2025-06-14 11:43:44 +0200

    Added asteroid field and container groups

--- main.py
+++ main.py
@@ -3,31 +3,49 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from os import sys
 
 
 def main():
-    pygame.init() # Initalize Pygame
+    pygame.init()  # Initalize Pygame
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = updateable
+    asteroidfield = AsteroidField()
+
     while True:
-        for event in pygame.event.get(): # Exit button
+        for event in pygame.event.get():  # Exit button
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen,(0,0,0)) # Black screen
-        pygame.display.flip() # Updates the screen or something
+        screen.fill((10, 50, 90))
+        updateable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        for obj in asteroids:
+            if obj.collision(player):
+                print("Game over!")
+                sys.exit()
 
+        pygame.display.flip()
 
-
-
-
-
-
-
-
-
-
-
+        # Set the FPS to 60
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
